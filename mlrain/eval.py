a325adbc1b54af94ecce3d6b77ff25ee83d6014c
xardas <sadsad@afa>
2024-11-21 13:11:44 +0100

    bug fix eval

--- mlrain/eval.py
+++ mlrain/eval.py
@@ -1,12 +1,13 @@
 from pandas import DataFrame
 from sklearn.metrics import confusion_matrix, classification_report
 
-from Module3.gruppenarbeit.mlrain.logger import init_logger
+from .logger import init_logger
 
 logger = init_logger(__name__)
 
 def evaluate_model(y_prediction: DataFrame, y_true_label: DataFrame):
 
 
-    logger.info("\n" + confusion_matrix(y_true_label, y_prediction))
-    logger.info("\n" + classification_report(y_true_label, y_prediction))
+    logger.info(f"\n  {confusion_matrix(y_true_label, y_prediction)}")
+    logger.info(f"\n {classification_report(y_true_label, y_prediction)}")
+
